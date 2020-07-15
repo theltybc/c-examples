ab78dd88d4a3bcf58a92b41887106b6dfa31d652
denis-l <0>
2020-07-15 09:09:15 +0500

    add write read bits(coils)

--- ex/modbus_client.c
+++ ex/modbus_client.c
@@ -16,7 +16,7 @@
 
 #define SERVER_ID 1
 #define ADDRESS_START 0
-#define ADDRESS_END 30
+#define ADDRESS_END 10
 
 /* At each loop, the program works in the range ADDRESS_START to
  * ADDRESS_END then ADDRESS_START + 1 to ADDRESS_END and so on.
@@ -26,13 +26,11 @@ int main(void) {
   int rc;
   int addr;
   int nb;
-  uint16_t *tab_rq_registers;
-  uint16_t *tab_rp_registers;
   unsigned error_count = 0;
   unsigned reqest_count = 0;
 
   /* RTU */
-  ctx = modbus_new_rtu("/dev/ttyUSB1", 115200, 'N', 8, 1);
+  ctx = modbus_new_rtu("/dev/ttyUSB0", 115200, 'N', 8, 1);
   modbus_set_slave(ctx, SERVER_ID);
 
   struct timeval tv = {0, 200 * 1000};
@@ -47,10 +45,16 @@ int main(void) {
   /* Allocate and initialize the different memory spaces */
   nb = ADDRESS_END - ADDRESS_START;
 
-  tab_rq_registers = (uint16_t *)malloc(nb * sizeof(uint16_t));
+  uint8_t *tab_rq_bits = (uint8_t *)malloc(nb * sizeof(uint8_t));
+  memset(tab_rq_bits, 0, nb * sizeof(uint8_t));
+
+  uint8_t *tab_rp_bits = (uint8_t *)malloc(nb * sizeof(uint8_t));
+  memset(tab_rp_bits, 0, nb * sizeof(uint8_t));
+
+  uint16_t *tab_rq_registers = (uint16_t *)malloc(nb * sizeof(uint16_t));
   memset(tab_rq_registers, 0, nb * sizeof(uint16_t));
 
-  tab_rp_registers = (uint16_t *)malloc(nb * sizeof(uint16_t));
+  uint16_t *tab_rp_registers = (uint16_t *)malloc(nb * sizeof(uint16_t));
   memset(tab_rp_registers, 0, nb * sizeof(uint16_t));
 
   while (errno != EIO) {
@@ -61,8 +65,24 @@ int main(void) {
       /* Random numbers (short) */
       for (i = 0; i < nb; i++) {
         tab_rq_registers[i] = (uint16_t)(65535.0 * rand() / (RAND_MAX + 1.0));
+        tab_rq_bits[i] = tab_rq_registers[i] % 2;
       }
       nb = ADDRESS_END - addr;
+
+      /* WRITE BIT */
+      rc = modbus_write_bit(ctx, addr, tab_rq_bits[0]);
+      if (rc != 1) {
+        printf("ERROR modbus_write_bit (%d) %s\n", rc, modbus_strerror(errno));
+        printf("Address = %d, value = %d\n", addr, tab_rq_bits[0]);
+        error_count++;
+      } else {
+        rc = modbus_read_bits(ctx, addr, 1, tab_rp_bits);
+        if (rc != 1 || tab_rq_bits[0] != tab_rp_bits[0]) {
+          printf("ERROR modbus_read_bits single (%d) %s\n", rc, modbus_strerror(errno));
+          printf("address = %d\n", addr);
+          error_count++;
+        }
+      }
 
       /* MULTIPLE REGISTERS */
       rc = modbus_write_registers(ctx, addr, nb, tab_rq_registers);
@@ -93,6 +113,7 @@ int main(void) {
     }
     printf("Request: %u; error: %u; %%: %f;\n", reqest_count, error_count, ((float)error_count / (float)reqest_count));
   }
+  perror("Exit loop");
 
   /* Free the memory */
   free(tab_rq_registers);
