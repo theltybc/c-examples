7e7ae8336dc33339d91972a36cced0dd3044ca60
denis-l <0>
2020-07-27 15:04:20 +0500

    add primes namber with openMP

--- ex/open_mp/open_mp.c
+++ ex/open_mp/open_mp.c
@@ -1,6 +1,21 @@
 #include "omp.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include <time.h>
+#include <unistd.h>
+
+static int is_prime(uint64_t num) {
+  uint64_t mid = (uint64_t)sqrt((double)num);
+  // printf("num: %lu mid: %lu \n", num, mid);
+  for (uint64_t i = 2; i <= mid; i++) {
+    if ((num % i) == 0) {
+      return 0;
+    }
+  }
+  return 1;
+}
 
 int main() {
   int value = 0b0;
@@ -44,6 +59,28 @@ int main() {
     }
   }
   printf("finish\n\n");
+
+  uint64_t count = 1000000;
+  uint64_t num = 1;
+  uint64_t primes[count];
+  uint64_t cur = 0;
+
+#pragma omp parallel for
+  for (num = 1; num < 10000000; num++) {
+    if (is_prime(num)) {
+#pragma omp critical(push)
+      {
+        primes[cur] = num;
+        cur++;
+      }
+    }
+  }
+
+  srand(time(NULL));
+#pragma omp parallel for
+  for (num = 0; num < cur; num++) {
+    assert(is_prime(primes[cur] * primes[rand() % cur]));
+  }
 
   return EXIT_SUCCESS;
 }
